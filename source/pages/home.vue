ba34d31ce8794dd0cf6a0d9570dd402c45dd4e73
Jens Obel <jensobel@me.com>
2017-10-18 20:14:13 +0200

    Updated button style

--- source/pages/home.vue
+++ source/pages/home.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="inner cover">
-    <p>Hello</p>
+    <p>
+      Ghost Writer helps you capture your life story through the digital artifacts you have left behind.<br><br> By matching your digital data with your personal memories, Ghost Writer will help you leave your memories behind in a meaningfull way.
+    </p>
+    <p class="lead">
+      <router-link to="/onboarding" id="action" class="btn btn-outline-light">Start writing</router-link>
+    </p>
   </div>
 </template>
 
 <style>
+#action {
+  color: #2c2863;
+  background-color: #fff;
+  padding: 15px 30px 15px 30px;
+}
+
+#action:hover {
+  color: #fff;
+  background-color: transparent;
+}
 </style>
